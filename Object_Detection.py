25666084785df831d519a044a22b1f5870e3a5ea
Nimesh55 <80301441+Nimesh55@users.noreply.github.com>
2022-05-12 23:55:08 +0530

    Update Object_Detection.py

--- Object_Detection.py
+++ Object_Detection.py
@@ -9,7 +9,16 @@ classes = []
 with open('coco.names','r') as f:
     classes = f.read().splitlines()
 
-cap =cv2.VideoCapture('test03.mp4')
+cap =cv2.VideoCapture('test3.mp4')
+
+# Setup initial positions in the line
+_, img = cap.read()
+height, width,_ = img.shape
+line_position = 3*height//4
+line_left = 90
+line_left_down = 25
+line_right = width-90
+line_right_down = width-25
 
 while True:
     _, img = cap.read()
@@ -49,6 +58,10 @@ while True:
     font = cv2.FONT_HERSHEY_PLAIN
     colors = np.random.uniform(0, 255, size=(len(boxes),3))
 
+    cv2.line(img, (line_left, line_position),(line_right, line_position),(255,127,0), 4)
+    cv2.line(img, (line_left, line_position),(line_left_down, line_position+25),(255,127,0), 4)
+    cv2.line(img, (line_right, line_position),(line_right_down, line_position+25),(255,127,0), 4)
+
     if len(indexes) > 0:
         for i in indexes.flatten():
             x,y,w,h = boxes[i]
@@ -56,12 +69,37 @@ while True:
             confidence = str(round(confidences[i],2))
             color = colors[i]
             cv2.rectangle(img, (x,y), (x+w,y+h), color, 2)
-            cv2.putText(img, label + " "+confidence, (x,y+20), font, 2, (255,255,255), 2)
+            cv2.putText(img, label, (x+w//2,y+h//2), font, 1, color, 1)
+
+            if(y<line_position and y+h>line_position):
+                cv2.putText(img, "Slow down vehicle", (width//6, height-10),font,3,(255,0,0),3)
 
     cv2.imshow('Image',img)
     key = cv2.waitKey(1)
     if key == 27:
         break
+    elif key == 119: #up
+        if line_position>0:
+            line_position -= 5
+    elif key == 115: #down
+        if line_position<height:
+            line_position += 5
+    elif key == 97: #left
+        line_left-=5
+    elif key == 65: #left reverse
+        line_left+=5
+    elif key == 122: #left down
+        line_left_down-=5
+    elif key == 90: #left down reverse
+        line_left_down+=5
+    elif key == 100: #right
+        line_right+=5
+    elif key == 68: #right reverse
+        line_right-=5
+    elif key == 99: #right down
+        line_right_down+=5
+    elif key == 67: #right down reverse
+        line_right_down-=5
 
 cap.release()
 cv2.destroyAllWindows()
